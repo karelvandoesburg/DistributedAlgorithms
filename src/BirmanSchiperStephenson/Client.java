944e5627a118d7332bc6e7eb62033015c4b3e096
Karel van Doesburg <karelvandoesburg@gmail.com>
2017-11-24 12:53:54 +0100

    baby steps in client and main

--- src/BirmanSchiperStephenson/Client.java
+++ src/BirmanSchiperStephenson/Client.java
@@ -1,5 +1,20 @@
 package BirmanSchiperStephenson;
 
-public class Client {
+public class Client implements Runnable {
+	
+	private int ID;
+	private int amountofprocesses;
+	
+	public Client(int ID, int amountofprocesses) {
+		this.ID = ID;
+		this.amountofprocesses = amountofprocesses;
+	}
+	
+	
+	public void run() {
+		
+		System.out.println("I am client: " + this.ID);
+		
+	}
 
 }
--- src/BirmanSchiperStephenson/Main.java
+++ src/BirmanSchiperStephenson/Main.java
@@ -4,20 +4,23 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
-import test.Tester;
-
 public class Main {
 
 	public static void main(String[] args) {
 		int port = 1099;
 		createLocalRegistry(port);
+		int amountofprocesses = 10;
 		
-		
+		for (int i = 0; i < amountofprocesses; i++) {
+			Client client = new Client(i+1,10);
+			new Thread(client).start();
+		}
 	}
 	
 	public static void createLocalRegistry(int port) {
 		try {
 			LocateRegistry.createRegistry(port);
+			System.out.println("Server ready!");
 		}
 		catch(Exception e) {
 			e.printStackTrace();
